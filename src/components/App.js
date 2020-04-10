a1e709056c365712ef889d033313a59ad6736b82
alex.fan <alex.fan@okchem.com>
2020-04-10 21:16:53 +0800

    send message to server

--- src/components/App.js
+++ src/components/App.js
@@ -5,62 +5,85 @@ const topic = "/foo";
 
 export default () => {
   const [state, setState] = useState("not ready");
+  const [client, setClient] = useState(null);
+  const [clientId, setClientId] = useState(null);
 
   useEffect(() => {
-    var clientId = "mqttjs_" + Math.random().toString(16).substr(2, 8);
+    if (client) {
+      client.on("error", function(err) {
+        console.log(err);
+        client.end();
+      });
+
+      client.on("connect", function() {
+        console.log("client connected:" + clientId);
+        setState("ready");
+      });
+
+      client.subscribe("topic", { qos: 0 });
+
+      client.publish("topic", "wss secure connection demo...!", {
+        qos: 0,
+        retain: false
+      });
+
+      client.on("message", function(topic, message, packet) {
+        console.log(
+          "Received Message:= " + message.toString() + "\nOn topic:= " + topic
+        );
+      });
+
+      client.on("close", function() {
+        console.log(clientId + " disconnected");
+      });
+    }
+  }, [client]);
+
+  useEffect(() => {
+    setClientId(
+      "mqttjs_" +
+        Math.random()
+          .toString(16)
+          .substr(2, 8)
+    );
 
     var host = "ws://localhost:3000";
 
-    // var options = {
-    //   keepalive: 10,
-    //   clientId: clientId,
-    //   protocolId: "MQTT",
-    //   protocolVersion: 4,
-    //   clean: true,
-    //   reconnectPeriod: 1000,
-    //   connectTimeout: 30 * 1000,
-    //   will: {
-    //     topic: "WillMsg",
-    //     payload: "Connection Closed abnormally..!",
-    //     qos: 0,
-    //     retain: false,
-    //   },
-    //   username: "demo",
-    //   password: "demo",
-    //   rejectUnauthorized: false,
-    // };
+    var options = {
+      keepalive: 10,
+      clientId: clientId,
+      protocolId: "MQTT",
+      protocolVersion: 4,
+      clean: true,
+      reconnectPeriod: 1000,
+      connectTimeout: 30 * 1000,
+      will: {
+        topic: "WillMsg",
+        payload: "Connection Closed abnormally..!",
+        qos: 0,
+        retain: false
+      },
+      username: "demo",
+      password: "demo",
+      rejectUnauthorized: false
+    };
 
-    var client = mqtt.connect(host);
-
-    client.on("error", function (err) {
-      console.log(err);
-      client.end();
-    });
-
-    client.on("connect", function () {
-      console.log("客户端连接到服务器成功! id:" + clientId);
-      setState("ready");
-    });
-
-    client.subscribe(topic, { qos: 0 });
-
-    // client.publish("topic", "wss secure connection demo...!", {
-    //   qos: 0,
-    //   retain: false,
-    // });
-
-    client.on("message", function (topic, message, packet) {
-      console.log(
-        "Received Message:= " + message.toString() + "\nOn topic:= " + topic
-      );
-    });
-
-    client.on("close", function () {
-      console.log(clientId + " disconnected");
-      setState("not ready");
-    });
+    setClient(mqtt.connect(host, options));
   }, []);
 
+  const onSend = () => {
+    if (client)
+      client.publish("topic", "blah blah.......", {
+        qos: 0,
+        retain: false
+      });
+  };
+
   if (state !== "ready") return <div>Connecting...</div>;
-  return <div>Hello MQTT!</div>;
+  return (
+    <div>
+      Hello MQTT!
+      <button onClick={onSend}>Send another message</button>
+    </div>
+  );
 };
